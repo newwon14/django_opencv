e681b27d47b4140a7aeb6e4b897469f5f48b4fae
newwon14 <cac2121@daum.net>
2021-02-08 15:31:05 +0900

    first commit

---
+++ opencv_webapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import ImageUploadModel
+
+
+class SimpleUploadForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    image = forms.ImageField()
+    #file = forms.FileField()
+
+class ImageUploadForm(forms.ModelForm):
+
+    class Meta:
+        model = ImageUploadModel
+        fields = ('description', 'document')
